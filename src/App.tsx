90d4dc58d47c6abb43005b858ce887764fdde852
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-10-10 08:50:09 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl text-yellow-500 underline bg-gradient-to-bl from-amber-300'>Netflix app is live now</h1>
+
+    </>
+  )
+}
+
+export default App
