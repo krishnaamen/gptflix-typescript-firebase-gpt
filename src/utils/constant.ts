29270f4a6e81ea70bfc854a67744dcedcb51ff02
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-10-13 13:12:01 +0200

    constant file

---
+++ src/utils/constant.ts
@@ -0,0 +1,2 @@
+export const NETFLIX_BG = 'https://assets.nflxext.com/ffe/siteui/vlv3/f272782d-cf96-4988-a675-6db2afd165e0/web/DK-en-20241008-TRIFECTA-perspective_8de77145-a891-4bd2-8f1f-d603aea8878a_large.jpg';
+export const NETFLIX_LOGO = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png';
