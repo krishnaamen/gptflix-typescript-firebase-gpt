9c601563393f527b7289290dc8ff15055c8c1c7d
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-10-16 08:05:04 +0200

    new bug fixes for build

--- src/components/GptSearch.tsx
+++ src/components/GptSearch.tsx
@@ -1,4 +1,3 @@
-import GptMovieSuggestion from "./GptMovieSuggestion"
 import GptSearchBar from "./GptSearchBar"
 const GptSearch = () => {
     return (
--- src/components/GptSearchBar.tsx
+++ src/components/GptSearchBar.tsx
@@ -1,13 +1,15 @@
 import { useSelector } from "react-redux"
 import lang from "../utils/langconstant"
-
 const GptSearchBar = () => {
-    const langKey = useSelector((store: { config: { lang: string } }) => store.config.lang);
+type LangKeys = keyof typeof lang;
+
+    const search = useSelector((store: { config: { lang: LangKeys } }) => store.config.lang);
+    
     return (
         <div className='pt-[10%] flex justify-center'>
             <form className='w-1/2 grid grid-cols-12 gap-2'>
-                <input type="text" placeholder={lang[langKey].placeHolder} className="bg-gray-800 text-white p-2 rounded-lg col-span-9" />
-                <button type="submit" className="bg-blue-500 p-2 rounded-lg col-span-3">{lang[langKey].search}</button>
+                <input type="text" placeholder={lang[search].placeHolder} className="bg-gray-800 text-white p-2 rounded-lg col-span-9" />
+                <button type="submit" className="bg-blue-500 p-2 rounded-lg col-span-3">{lang[search].search}</button>
             </form>
 
         </div>
